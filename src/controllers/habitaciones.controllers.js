0fb283b8f3657b5d4c814025e9c24e8ca3b2e738
JFranco-2020338 <101079444+JFranco-2020338@users.noreply.github.com>
2022-07-01 00:12:11 -0600

    Habitaciones back

--- src/controllers/habitaciones.controllers.js
+++ src/controllers/habitaciones.controllers.js
@@ -1,4 +1,5 @@
 const Habitaciones = require('../models/habitaciones.model');
+const Hoteles = require('../models/hotel.model');
 
 //Ver habitaciones
 function ObtenerHabitaciones(req, res) {
@@ -7,6 +8,19 @@ function ObtenerHabitaciones(req, res) {
         return res.status(200).send({ habitaciones: habitacionesEncontrados })
     })
 }
+
+//Obtener una solo habitación
+function ObtenerHabitacionId(req, res) {
+    const idHab = req.params.idHabitacion;
+
+    Habitaciones.findById(idHab, (err, habitacionEncontrado) => {
+        if (err) return res.status(500).send({ mensaje: 'Erro en la peticion' });
+        if (!habitacionEncontrado) return res.status(500).send({ mensaje: 'Error al obtener la habitacion' });
+
+        return res.status(200).send({ habitacion: habitacionEncontrado });
+    })
+}
+
 
 // Agregar habitaciones
 function AgregarHabitaciones(req, res) {
@@ -18,7 +32,7 @@ function AgregarHabitaciones(req, res) {
         modeloHabitaciones.tipoHabitacion = parametros.tipoHabitacion;
         modeloHabitaciones.numeroPiso = parametros.numeroPiso;
         modeloHabitaciones.precio = parametros.precio;
-        modeloHabitaciones.idHoteles = req.user.sub;
+        //modeloHabitaciones.idHoteles = req.user.sub;
 
         modeloHabitaciones.save((err, habitacionGuardado) => {
 
@@ -64,5 +78,8 @@ module.exports = {
     ObtenerHabitaciones,
     AgregarHabitaciones,
     EditarHabitaciones,
-    EliminarHabitaciones
+    EliminarHabitaciones,
+    ObtenerHabitacionId
 }
+
+//Rep antiguo
