66df068e21e79357c246d8c1ab9ca66c4a02992a
Edwar Esaul López Pérez <88805623+elopez2020236@users.noreply.github.com>
2022-07-01 15:26:31 -0600

    edw

--- src/routes/eventos.routes.js
+++ src/routes/eventos.routes.js
@@ -5,9 +5,9 @@ const md_autenticacion = require("../middlewares/autenticacion");
 
 const api = express.Router();
 
-api.post("/verEventos",  eventosController.ObtenerEventos);
-api.post("/agregarEvento",md_autenticacion.Auth,eventosController.AgregarEventos);
-api.put("/editarEvento/:idEvento",  eventosController.EditarEventos);
-api.delete("/eliminarEvento/:idEvento",  eventosController.EliminarEventos);
+api.post("/verEventos",md_autenticacion.Auth,  eventosController.ObtenerEventos);
+api.post("/agregarEvento",md_autenticacion.Auth, eventosController.AgregarEventos);
+api.put("/editarEvento/:idEvento", md_autenticacion.Auth, eventosController.EditarEventos);
+api.delete("/eliminarEvento/:idEvento", md_autenticacion.Auth, eventosController.EliminarEventos);
 
 module.exports = api;
--- src/routes/habitaciones.routes.js
+++ src/routes/habitaciones.routes.js
@@ -5,11 +5,11 @@ const md_autenticacion = require("../middlewares/autenticacion");
 
 const api = express.Router();
 
-api.get('/verHabitaciones', hotelesController.ObtenerHabitaciones);
+api.get('/verHabitaciones', md_autenticacion.Auth,hotelesController.ObtenerHabitaciones);
 api.post('/agregarHabitacion', md_autenticacion.Auth, hotelesController.AgregarHabitaciones);
-api.put('/editarHabitacion/:idHabitacion', hotelesController.EditarHabitaciones);
-api.delete('/eliminarHabitacion/:idHabitacion', hotelesController.EliminarHabitaciones);
-api.get('/habitacion/:idHabitacion', hotelesController.ObtenerHabitacionId);
+api.put('/editarHabitacion/:idHabitacion',md_autenticacion.Auth, hotelesController.EditarHabitaciones);
+api.delete('/eliminarHabitacion/:idHabitacion', md_autenticacion.Auth,hotelesController.EliminarHabitaciones);
+api.get('/habitacion/:idHabitacion', md_autenticacion.Auth,hotelesController.ObtenerHabitacionId);
 
 module.exports = api;
 
