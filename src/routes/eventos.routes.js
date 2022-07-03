7dc2f6c0121078041752f1bfbc5b66c0671bea21
Genrry Uwualdo Luis Alvarado <88805705+gluis2020232@users.noreply.github.com>
2022-07-02 20:45:02 -0600

    Commit GLuis

--- src/routes/eventos.routes.js
+++ src/routes/eventos.routes.js
@@ -5,9 +5,9 @@ const md_autenticacion = require("../middlewares/autenticacion");
 
 const api = express.Router();
 
-api.post("/verEventos",  eventosController.ObtenerEventos);
-api.post("/agregarEvento",  eventosController.AgregarEventos);
-api.put("/editarEvento/:idEvento",  eventosController.EditarEventos);
-api.delete("/eliminarEvento/:idEvento",  eventosController.EliminarEventos);
+api.post("/verEventos", eventosController.ObtenerEventos);
+api.post("/agregarEvento", eventosController.AgregarEventos);
+api.put("/editarEvento/:idEvento", eventosController.EditarEventos);
+api.delete("/eliminarEvento/:idEvento", eventosController.EliminarEventos);
 
 module.exports = api;
