2925d56417cfdf46b669da9739dbabb2a622847b
Genrry Uwualdo Luis Alvarado <88805705+gluis2020232@users.noreply.github.com>
2022-07-03 01:46:31 -0600

    Commit Evento

--- src/controllers/eventos.controller.js
+++ src/controllers/eventos.controller.js
@@ -33,8 +33,8 @@ function AgregarEventos(req, res) {
         modeloEventos.fecha = parametros.fecha;
         modeloEventos.asistentes = parametros.asistentes;
 
-        modeloEventos.idHotel = req.user.sub;
-        modeloEventos.idTipoEvento = req.user.sub;
+        //modeloEventos.idHotel = req.user.sub;
+        //modeloEventos.idTipoEvento = req.user.sub;
 
 
         modeloEventos.save((err, eventoGuardado) => {
