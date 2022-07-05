e71240bde93c60365904c54347bfe548acbf50e6
Genrry Uwualdo Luis Alvarado <88805705+gluis2020232@users.noreply.github.com>
2022-07-04 20:41:58 -0600

    Habilitar auth en /verHoteles

--- src/routes/hotel.routes.js
+++ src/routes/hotel.routes.js
@@ -9,7 +9,7 @@ const api = express.Router();
 api.post("/agregarHotel", [md_autenticacion.Auth, md_rol.verAdmin], hotelesController.agregarHotel);
 api.put("/editarHotel/:idHotel", md_autenticacion.Auth, hotelesController.editarHotel);
 api.delete("/eliminarHotel/:id", [md_autenticacion.Auth, md_rol.verAdmin], hotelesController.eliminarHotel);
-api.get("/verHoteles", md_autenticacion.Auth, hotelesController.verHoteles);
+api.get("/verHoteles", [md_autenticacion.Auth, md_rol.verAdmin], hotelesController.verHoteles);
 api.get("/verHotelesId/:idHotel", [md_autenticacion.Auth, md_rol.verAdmin], hotelesController.obtenerHotelesById);
 api.get("/buscarHotelByName", md_autenticacion.Auth, hotelesController.buscarHotelByName);
 
