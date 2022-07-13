2e352ab448573be0bdbd5cbbef26f81bfa29f053
slopez2020276 <slopez-2020276@kinal.edu.gt>
2022-07-12 18:10:13 -0600

    ª

--- src/models/hotel.model.js
+++ src/models/hotel.model.js
@@ -6,8 +6,8 @@ const HotelSchema = mongoose.Schema({
   telefono: String,
   idGerente: { type: Schema.Types.ObjectId, ref: "Usuarios" },
   Habitaciones: [{ type: Schema.Types.ObjectId, ref: 'Habitaciones'}],
-  Servios:[{ type: Schema.Types.ObjectId, ref: 'Servicios'}]
-
+  Servios:[{ type: Schema.Types.ObjectId, ref: 'Servicios'}],
+  Eventos:[{ type: Schema.Types.ObjectId, ref: 'Eventos'}]
 });
 
 module.exports = mongoose.model("Hotel", HotelSchema);
